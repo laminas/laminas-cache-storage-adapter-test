6675654f3a961679008d03ebaf592cf9caf964a0
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2021-08-02 11:34:16 +0200

    feature: add integration test for component installer
    Provide a test trait to verify that the `composer.json` of a cache adapters is providing the necessary informations for projects using the `laminas-component-installer` component.
    
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

---
+++ test/TestAsset/ConfigProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LaminasTestTest\Cache\Storage\Adapter\TestAsset;
+
+final class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [];
+    }
+}
