ea0eaeef4c534249b51dfc2480d0461520cbec80
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2024-06-21 21:21:30 +0200

    bugfix: ensure we do have proper storage assertions
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

--- test/integration/AdapterForIntegrationTestDetector.php
+++ test/integration/AdapterForIntegrationTestDetector.php
@@ -8,11 +8,12 @@ use Composer\InstalledVersions;
 use Laminas\Cache\Exception\RuntimeException;
 use Laminas\Cache\Storage\Adapter\Apcu;
 use Laminas\Cache\Storage\Adapter\Memory;
+use Laminas\Cache\Storage\FlushableInterface;
 use Laminas\Cache\Storage\StorageInterface;
+use Webmozart\Assert\Assert;
 
 use function assert;
 use function class_exists;
-use function is_a;
 
 final class AdapterForIntegrationTestDetector
 {
@@ -24,9 +25,10 @@ final class AdapterForIntegrationTestDetector
     {
         if (InstalledVersions::isInstalled('laminas/laminas-cache-storage-adapter-apcu')) {
             assert(class_exists(Apcu::class));
-            assert(is_a(Apcu::class, StorageInterface::class));
+            Assert::implementsInterface(Apcu::class, StorageInterface::class);
+            Assert::implementsInterface(Apcu::class, FlushableInterface::class);
             /**
-             * @psalm-suppress MixedReturnStatement Due to recursive dependencies, we can not have APCu installed as
+             * @psalm-suppress MixedReturnStatement Due to recursive dependencies, we can not have APCu installed
              *                                        during development. Will be installed during CI via
              *                                        `.laminas-ci.json`.
              */
@@ -35,9 +37,10 @@ final class AdapterForIntegrationTestDetector
 
         if (InstalledVersions::isInstalled('laminas/laminas-cache-storage-adapter-memory')) {
             assert(class_exists(Memory::class));
-            assert(is_a(Memory::class, StorageInterface::class));
+            Assert::implementsInterface(Memory::class, StorageInterface::class);
+            Assert::implementsInterface(Memory::class, FlushableInterface::class);
             /**
-             * @psalm-suppress MixedReturnStatement Due to recursive dependencies, we can not have Memory installed as
+             * @psalm-suppress MixedReturnStatement Due to recursive dependencies, we can not have Memory installed
              *                                        during development. Will be installed during CI via
              *                                        `.laminas-ci.json`.
              */
